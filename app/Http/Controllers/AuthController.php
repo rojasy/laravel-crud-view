cf973c76951ce4f1522e1119791ee8242ad1bbd3
rojasy <rojasyngaiza01@gmail.com>
2025-06-24 16:39:12 +0300

    login functionality

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,12 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
     public function register(Request $request)
     {
-        dd('Ok');
+        $fields = $request->validate([
+            'username' => ['required', 'max:255'],
+            'email' => ['required', 'email', 'max:255','unique:users'],
+            'password' => ['required', 'confirmed', 'min:3'],
+        ]);
+
+        $user = User::create($fields);
+
+        Auth::login($user);
+
+        return redirect()->route('home');
     }
+
+
+    public function login(Request $request){
+        $fields = $request->validate([
+            'email' => ['required', 'email', 'max:255'],
+            'password' => ['required'],
+        ]);
+
+        if(Auth::attempt($fields,$request->remember)){
+            return redirect()->intended();
+        }else{
+            return back()->withErrors([
+                'failed' => 'The provided credentials do not match our records.'
+            ]);
+        }
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,5 @@ Route::get('/', function () {
 Route::view('/register','auth.register')->name('register'); // this is for view/ displaying page
 
 Route::post('/register',[AuthController::class,'register']); // this is for taking/post data from form
+Route::view('/login','auth.login')->name('login'); // this is for view/ displaying page
+Route::post('/login',[AuthController::class,'login']); // this is for taking/post data from form
