4c009f7e1be8dbdf247a2db757014c1ca7db44c8
rojasy <rojasyngaiza01@gmail.com>
2025-06-26 15:10:30 +0300

    middleware in routes

--- routes/web.php
+++ routes/web.php
@@ -6,13 +6,21 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'posts.index')->name('home');
 
-Route::get('/dashboard',[DashboardController::class,'index'])
-    ->name('dashboard');
+Route::middleware('auth')->group(function (){
+    Route::get('/dashboard',[DashboardController::class,'index'])
+        ->name('dashboard');
 
-Route::post('/logout',[AuthController::class,'logout'])->name('logout');
+    Route::post('/logout',[AuthController::class,'logout'])->name('logout');
 
-Route::view('/register','auth.register')->name('register'); // this is for view/ displaying page
+});
 
-Route::post('/register',[AuthController::class,'register']); // this is for taking/post data from form
-Route::view('/login','auth.login')->name('login'); // this is for view/ displaying page
-Route::post('/login',[AuthController::class,'login']); // this is for taking/post data from form
+
+Route::middleware('guest')->group(function (){
+
+    Route::view('/register','auth.register')->name('register'); // this is for view/ displaying page
+
+    Route::post('/register',[AuthController::class,'register']); // this is for taking/post data from form
+    Route::view('/login','auth.login')->name('login'); // this is for view/ displaying page
+    Route::post('/login',[AuthController::class,'login']); // this is for taking/post data from form
+
+});
